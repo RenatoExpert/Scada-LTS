122f619d81b51363cd02e91db884aa40eba27671
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-23 16:46:51 -0300

    implementing high performance library

--- WebContent/assets/hp/main.js
+++ WebContent/assets/hp/main.js
@@ -17,21 +17,26 @@ var loaded;
 var generated;
 var current_view;
 
+function find_parent(view_name) {
+	let l2_list = loaded.tree.root.children;
+	let process = {};
+	for(let l2 in l2_list) {
+		let process = l2_list[l2];
+		let children = process.children;
+		if(view_name in children) {
+			return process;
+		}
+	}
+	console.error('L3 name not found', view_name);
+}
+
 function get_current_view() {
 	let view = {};
-	get_id: {
+	get_xid: {
 		let queryString = window.location.search;
 		let urlParams = new URLSearchParams(queryString);
-		let id = urlParams.get('viewId');
-		view.id = id || 0;
-	}
-	console.log(view);
-	get_xid: {
-		for(let xid in loaded.links) {
-			if(loaded.links[xid] == view.id) {
-				view.xid = xid;
-			}
-		}
+		let xid = urlParams.get('xid') || "l0-banner";
+		view.xid = xid;
 	}
 	split: {
 		let split = view.xid.split("-");
@@ -40,6 +45,9 @@ function get_current_view() {
 	}
 	from_tree: {
 		switch(view.level) {
+			case 'l0':
+				console.warn("No XID detected. Lets consider L0");
+				break;
 			case 'l1':
 				process = loaded.tree.root.menus[view.name];
 				view.process = process;
@@ -53,25 +61,18 @@ function get_current_view() {
 				view.title = process.label;
 				break;
 			case 'l3':
-				let l2_list = loaded.tree.root.children;
-				for(let l2 in l2_list) {
-					let process = l2_list[l2];
-					let children = process.children;
-					if(view.name in children) {
-						view.process = process;
-						let reference = children[view.name];
-						view.label = reference.label;
-						view.title = process.label + "/" + view.label; 
-						console.log({ l2, process });
-						break from_tree;
-					}
-				}
-				console.error('L3 name not found', view.name);
+				process = find_parent(view.name);
+				view.process = process;
+				let reference = process.children[view.name];
+				view.label = reference.label;
+				view.title = process.label + "/" + view.label; 
+				console.log({ process });
 				break;
 			default:
 				console.error('Level may be l1, l2 or l3, but its', view.level);
 		}
 	}
+	console.log({view});
 	return view;
 }
 
@@ -101,93 +102,149 @@ async function main() {
 			localhost: localhost_path,
 			preferred: preferred_path,
 			l1: `${preferred_path}/Scada-LTS/assets/hp/header_L1.svg`,
-			tree: `${preferred_path}/Scada-LTS/assets/hp/views_tree.js`,
-			links: `${preferred_path}/Scada-LTS/assets/hp/views_link.js`
+			tree: `${preferred_path}/Scada-LTS/assets/hp/views_tree.js`
 		};
 		console.log(sources);
 
-		[l1_svg, tree, links] = await Promise.all([ 
+		[l1_svg, tree] = await Promise.all([ 
 			load_svg(sources.l1),
-			load_json(sources.tree),
-			load_json(sources.links)
+			load_json(sources.tree)
 		]);
-		loaded = { l1_svg, tree, links };
+		loaded = { l1_svg, tree };
 	}
 
 	generate: {
 		let hp_headers = (() => {
-			div = document.createElement("div");
+			let div = document.createElement("div");
 			div.style.position = "absolute";
 			div.id = "hp-headers";
 			div.style.display = "grid";
+			let subtitle = document.createElement("div");
+			subtitle.id = "hp-headers-subtitle";
+			subtitle.className = "labelDiv";
+			subtitle.style.position = "absolute";
+			subtitle.style.display = "none";
+			subtitle.style.left = "200px";
+			subtitle.style.top = "200px";
+			div.appendChild(subtitle);
 			return div;
 		})();
 		current_view = get_current_view();
-		console.log({ current_view });
-		let l1 = (() => {
-			let div = loaded.l1_svg;
-			static_strings: {
-				div.querySelector("#process-title").innerHTML = current_view.title;
-				div.querySelector("#operator-name").innerHTML = "João Carlos";
-			}
-			bind_buttons: {
-				function bind_button(id, action) {
-					div.querySelector(`#${id}`).onclick = action;
+		if(current_view.level == 'l0') {
+			generated = { current_view };
+			// show banner
+		} else {
+			console.log({ current_view });
+			let l1 = (() => {
+				let div = loaded.l1_svg;
+				static_strings: {
+					div.querySelector("#process-title").innerHTML = current_view.title;
+					div.querySelector("#operator-name").innerHTML = "Luiz Fernando";
 				}
-				bind_button("l1-map", () => {
-					goto_id(loaded.links["l1-cidades"]);
-				});
-				bind_button("l1-system", () => {
-					goto_id(loaded.links["l1-estatisticas"]);
-				});
-				bind_button("l1-refresh", () => {
-					location.reload();
-				});
-				bind_button("l1-mute", () => {
-					alert("Sound muted!");
-				});
-				bind_button("l1-lock", () => {
-					alert("Logged out!");
-				});
-			}
-			return div;
-		})();
-		let l2 = create_inline_menu(loaded.tree.root.children, "l2");
-		let l3 = current_view.level == "l2" || current_view.level == "l3" ? create_inline_menu(current_view.process.children, "l3") : document.createElement("div");
+				bind_buttons: {
+					function bind_button(id, tooltip, action) {
+						let btn = div.querySelector(`#${id}`);
+						btn.onclick = action;
+						btn.onmouseover = () => {
+							subtitle(tooltip, btn);
+						};
+						btn.onmouseout = () => {
+							subtitle();
+						};
+					}
+					function subtitle(desc, source) {
+						var c = document.getElementById("hp-headers-subtitle");
+						if (desc) {
+							//var bounds = getAbsoluteNodeBounds(source);
+							let bounds = source.getBoundingClientRect();
+							console.log(bounds);
+							c.innerHTML = desc;
+							c.style.left = (bounds.x + 16) +"px";
+							c.style.top = (bounds.y - 150) +"px";
+							show(c);
+						} else {
+							hide(c);
+						}
+					}
+					bind_button("l1-map", "Mapa das estacoes", () => {
+						goto_id("l1-cidades");
+					});
+					bind_button("l1-system", "Status do sistema",() => {
+						goto_id("l1-estatisticas");
+					});
+					bind_button("l1-refresh", "Atualizar tela", () => {
+						location.reload();
+					});
+					bind_button("l1-mute", "Silenciar alarmes", () => {
+						alert("Sound muted!");
+					});
+					bind_button("l1-login", "Entrar", () => {
+						window.location.href = "login.htm";
+					});
+					bind_button("l1-lock", "Sair", () => {
+						window.location.href = "logout.htm";
+					});
+				}
+				return div;
+			})();
+			let l2 = create_inline_menu(loaded.tree.root.children, "l2");
+			let l3 = current_view.level == "l2" || current_view.level == "l3" ? create_inline_menu(current_view.process.children, "l3") : document.createElement("div");
+			let summary = current_view.level == "l2" ? create_status_table(current_view.process.children) : document.createElement("div");
 
-		l1.id = "header-l1";
-		l2.id = "header-l2";
-		l3.id = "header-l3";
+			l1.id = "header-l1";
+			l2.id = "header-l2";
+			l3.id = "header-l3";
+			summary.id = "summary";
 
-		generated = { hp_headers, l1, l2, l3, current_view };
+			generated = { hp_headers, l1, l2, l3, summary, current_view };
+		}
 	}
 
 	render: {
-		canvas.insertBefore(generated.hp_headers, viewBackground);
-		let headers = document.getElementById("hp-headers");
-		headers.appendChild(generated.l1);
-		if(current_view.level == "l2" || current_view.name == "cidades") {
-			headers.appendChild(generated.l2);
-			headers.appendChild(generated.l3);
+		if(current_view.level == "l0") {
+			setTimeout(() => {
+				goto_id("l1-estatisticas");
+			}, 10e3);
+		} else {
+			canvas.insertBefore(generated.hp_headers, viewBackground);
+			let headers = document.getElementById("hp-headers");
+			headers.appendChild(generated.l1);
+			if(current_view.level == "l2" || current_view.name == "cidades") {
+				headers.appendChild(generated.l2);
+				headers.appendChild(generated.l3);
+				headers.appendChild(generated.summary);
+			}
 		}
 		background: {
-			let extension = "png";
-			let src = `${sources.localhost}/Scada-LTS/assets/hp_bg/${generated.current_view.xid}.${extension}`;
-			if(await test_url(src)) {
-				change_background(src);
+			if(generated.current_view.level == "l0") {
+				let extension = "svg";
+				let src = `${sources.localhost}/Scada-LTS/assets/hp_bg/${generated.current_view.xid}.${extension}`;
+				if(await test_url(src)) {
+					change_background(src);
+				} else {
+					console.warn("Background file not found");
+				}
 			} else {
-				console.warn("Background file not found");
+				let extension = "png";
+				let src = `${sources.localhost}/Scada-LTS/assets/hp_bg/${generated.current_view.xid}.${extension}`;
+				if(await test_url(src)) {
+					change_background(src);
+				} else {
+					console.warn("Background file not found");
+				}
 			}
 		}
 	}
 
 	loop: {
-		setInterval(() => {
-			clock_update: {
-				change_text("date", get_date());
-				change_text("time", get_time());
-			}
-		}, 200);
+		if (current_view.level != "l0") {
+			setInterval(() => {
+				clock_update: {
+					change_text("date", get_date());
+					change_text("time", get_time());
+				}
+			}, 200);
+		}
 	}
 }
 
@@ -207,7 +264,7 @@ async function load_json(url) {
 }
 
 function goto_id(id) {
-	let link = `?viewId=${id}`;
+	let link = `?xid=${id}`;
 	window.location.href = link;
 }
 
@@ -216,13 +273,13 @@ function create_inline_menu(table, level) {
 	menu.style.display = "inline-flex";
 	menu.style.width = "1280px";
 	menu.style.flexWrap = "wrap";
+	menu.style.padding = "0px 5px";
 	for(item in table) {
 		let button = document.createElement("button");
 		button.textContent = table[item].label;
 		let xid = `${level}-${item}`;
-		let view_id = loaded.links[xid];
 		button.onclick = () => {
-			goto_id(view_id);
+			goto_id(xid);
 		};
 		button.id = `${level}-button-${item}`;
 		button.style.width = "90px";
@@ -230,6 +287,54 @@ function create_inline_menu(table, level) {
 		menu.append(button);
 	}
 	return menu;
+}
+
+function tag_text(tag, text) {
+	let element = document.createElement(tag);
+	element.innerHTML = text;
+	return element;
+}
+
+function format_var(value, eu) {
+	return `<b>${value}</b><small>${eu}</small>`;
+}
+
+function create_status_table(tree_table) {
+	let div = document.createElement("div");
+	let table = document.createElement("table");
+	table.className = "summary";
+	table.style.backgroundColor = "#f1f1f1";
+	header: {
+		let row = document.createElement("tr");
+		let headers = ["Estacao", "Modem", "Comunicacao", "Atualizacao", "Entrada", "Saida", "Temperatura", "Vazao", "Mes", "Anterior"];
+		for(header in headers) {
+			let col = tag_text("th", headers[header]);
+			row.append(col);
+		}
+		table.append(row);
+	}
+	for(item in tree_table) {
+		let row = document.createElement("tr");
+		let cols = [tree_table[item].label, "Conectado", "OK", "14:23",
+			format_var(22, "Kgf/cm²"),
+			format_var(12, "Kgf/cm²"),
+			format_var(28, "°C"),
+			format_var(4.792, "m3/dia"),
+			format_var(40.634, "m3"),
+			format_var(92.179, "m3")
+		];
+		for(i in cols) {
+			let col = tag_text("td", cols[i]);
+			row.append(col);
+		}
+		let xid = `l3-${item}`;
+		row.onclick = () => {
+			goto_id(xid);
+		};
+		table.append(row);
+	}
+	div.append(table);
+	return div;
 }
 
 //	Header labels
@@ -276,5 +381,4 @@ boot: {
 		main();
 	}
 }
-
 
