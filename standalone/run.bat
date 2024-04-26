fc3dec1d2ca6c210836b1b65a7d861c620deaf92
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-26 20:27:18 -0300

    test connection and set password in different stages

--- standalone/run.bat
+++ standalone/run.bat
@@ -5,14 +5,21 @@ set CATALINA_HOME=%ARGOS_HOME%\tomcat
 ./mysql/bin/mysqld.exe --user=root --initialize-insecure
 start /b ./mysql/bin/mysqld.exe --user=root --port=3306 --console
 
+:change_password
 echo ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'; > DEFINE_PASSWORD
+for /l %%x in (1, 1, 100) do (
+	echo Trying to change root password... %%x
+	./mysql/bin/mysql.exe -u root --skip-password < DEFINE_PASSWORD
+	if %errorlevel% eq 0 goto :test_password
+	ping localhost > nul
+)
+
+:test_password
 echo SELECT 1; > PING_SQL
 for /l %%x in (1, 1, 100) do (
 	echo Testing Mysql DB Connection... %%x
 	./mysql/bin/mysql.exe -u root --password=root < PING_SQL
 	if %errorlevel% eq 0 goto :start_tomcat
-	./mysql/bin/mysql.exe -u root --skip-password < DEFINE_PASSWORD
-	if %errorlevel% eq 0 echo "Password set to 'root'"
 	ping localhost > nul
 )
 
