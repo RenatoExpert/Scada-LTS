dc9cab22bac7b2047fd85b300494e4a2f7003c4f
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-26 17:58:35 -0300

    goto :start_tomcat

--- standalone/run.bat
+++ standalone/run.bat
@@ -8,11 +8,14 @@ set DEFINE_PASSWORD="ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
 set PING_SQL="SELECT 1;"
 for /l %%x in (1, 1, 100) do (
 	echo Testing Mysql DB Connection... %%x
-	./mysql/bin/mysql.exe -u root --password=root < %PING_SQL%		&& goto :eof
+	./mysql/bin/mysql.exe -u root --password=root < %PING_SQL%		&& goto :start_tomcat
 	./mysql/bin/mysql.exe -u root --skip-password < %DEFINE_PASSWORD%	&& echo "Password set to 'root'"
 	timeout 5;
 )
+echo "Failed to connect to mysql server";
+exit
 
+:start_tomcat
 echo "Starting Tomcat"
 call %CATALINA_HOME%\bin\catalina.bat run
 
