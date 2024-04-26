8caa299a061841f3885ed32640a1a3405bac7c15
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-26 17:09:52 -0300

    make a for loop to define password on mysql database

--- standalone/run.bat
+++ standalone/run.bat
@@ -2,5 +2,17 @@ set ARGOS_HOME=%cd%
 set JAVA_HOME=%ARGOS_HOME%\jdk
 set CATALINA_HOME=%ARGOS_HOME%\tomcat
 
+start /b ./mysql/bin/mysqld.exe --user=root --port=3306 --initialize-insecure
+
+set DEFINE_PASSWORD="ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
+set PING_SQL="SELECT 1;"
+for /l %%x in (1, 1, 100) do (
+	echo Testing Mysql DB Connection... %%x
+	./mysql/bin/mysql.exe -u root --password=root < %PING_SQL%		&& goto :eof
+	./mysql/bin/mysql.exe -u root --skip-password < %DEFINE_PASSWORD%	&& echo "Password set to 'root'"
+	timeout 5;
+)
+
+echo "Starting Tomcat"
 call %CATALINA_HOME%\bin\catalina.bat run
 
