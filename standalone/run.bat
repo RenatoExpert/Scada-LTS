23295602ca4e541919c948140316cbcf477a5d3e
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-29 10:34:57 -0300

    rewrite batch script

--- standalone/run.bat
+++ standalone/run.bat
@@ -1,26 +1,31 @@
-./mysql/bin/mysqld.exe --user=root --initialize-insecure
+mysql\bin\mysqld.exe --user=root --initialize-insecure
 echo "Database generated with no password"
 
-start /b ./mysql/bin/mysqld.exe --user=root --port=3306 --console
+start /b .\mysql\bin\mysqld.exe --user=root --port=3306 --console
 
 echo ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'; > DEFINE_PASSWORD
-for /L %%x in (1, 1, 100) do (
-	echo Trying to change root password... %%x
-	./mysql/bin/mysql.exe -u root --skip-password < DEFINE_PASSWORD
-	if %errorlevel% LEQ 1 %%x=100
-	ping localhost > nul
-)
-
 echo SELECT 1; > PING_SQL
-for /L %%x in (1, 1, 100) do (
-	echo Testing Mysql DB Connection... %%x
-	./mysql/bin/mysql.exe -u root --password=root < PING_SQL
-	if %errorlevel% LEQ 1 %%x=100
+:testconn
 	ping localhost > nul
-)
+	echo Loop [%%x]
+	echo Trying to connect with password...
+	type PING_SQL | .\mysql\bin\mysql.exe -u root --password=root
+	IF %errorlevel% LSS 1 (goto:runtomcat) ELSE (echo Does not connect with password)
+	echo Trying to connect without password...
+	type PING_SQL | .\mysql\bin\mysql.exe -u root
+	IF %errorlevel% LSS 1 (goto:changepassword) ELSE (echo Does not connect without password)
+	goto:testconn
 
-set ARGOS_HOME=%cd%
-set JAVA_HOME=%ARGOS_HOME%\jdk
-set CATALINA_HOME=%ARGOS_HOME%\tomcat
-call %CATALINA_HOME%\bin\catalina.bat run
+:changepassword
+	echo Trying [%%x] to change root password...;
+	type DEFINE_PASSWORD | .\mysql\bin\mysql.exe -u root
+	IF %errorlevel% LSS 1 (echo Password changed!) ELSE (echo Password changing failed)
+	goto:testconn
+
+:runtomcat
+	set ARGOS_HOME=%cd%
+	set JAVA_HOME=%ARGOS_HOME%\jdk
+	set CATALINA_HOME=%ARGOS_HOME%\tomcat
+	call %CATALINA_HOME%\bin\catalina.bat run
+
 
