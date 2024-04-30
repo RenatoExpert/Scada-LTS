8dab7dc94b03264c328f30e4f609399d2310be66
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-30 15:04:01 -0300

    making all going into a single command

--- standalone/run.bat
+++ standalone/run.bat
@@ -10,39 +10,15 @@ mysql\bin\mysqld.exe --user=root		^
 	--defaults-file=%ARGOS_HOME%\my.cnf	^
 	--init-file=%ARGOS_HOME%\scadalts.sql	^
 	--initialize-insecure			^
+	--port=3306				^
 	--console
-echo "Database generated with no password"
-
-start /b .\mysql\bin\mysqld.exe --user=root --port=3306 --console
+echo "Database initialized"
 
 :testconn
-	echo SELECT 1; > PING_SQL
 	ping localhost > nul
 	echo Trying to connect with database...
-	type PING_SQL | .\mysql\bin\mysql.exe -u root --password=root --database=scadalts
+	echo SELECT 1; | .\mysql\bin\mysql.exe -u root --password=root --database=scadalts
 	IF %errorlevel% LSS 1 (goto:runtomcat) ELSE (echo Does not connect with database)
-	echo Trying to connect with password...
-	type PING_SQL | .\mysql\bin\mysql.exe -u root --password=root
-	IF %errorlevel% LSS 1 (goto:createdb) ELSE (echo Does not connect with password)
-	echo Trying to connect without password...
-	type PING_SQL | .\mysql\bin\mysql.exe -u root
-	IF %errorlevel% LSS 1 (goto:changepassword) ELSE (echo Does not connect without password)
-	goto:testconn
-
-:changepassword
-	echo ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'; > DEFINE_PASSWORD
-	echo Trying to change root password...;
-	type DEFINE_PASSWORD | .\mysql\bin\mysql.exe -u root
-	IF %errorlevel% LSS 1 (echo Password changed!) ELSE (echo Password changing failed)
-	goto:testconn
-
-:createdb
-	echo CREATE DATABASE IF NOT EXISTS scadalts; > CREATE_DB
-	type CREATE_DB | .\mysql\bin\mysql.exe -u root --password=root
-	IF %errorlevel% LSS 1 (echo Database created!) ELSE (echo Database creation failed)
-	echo GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION;FLUSH PRIVILEGES; > GRANT
-	type GRANT | .\mysql\bin\mysql.exe -u root --password=root --database=scadalts
-	IF %errorlevel% LSS 1 (echo Privileges granted!) ELSE (echo Grant command failed)
 	goto:testconn
 
 :runtomcat
