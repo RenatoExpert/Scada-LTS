bc016e73a8231b9baf63edddf8784f164dac1328
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-05-20 19:59:36 -0300

    replacing opcua lib by asyncua

--- opcua_server/__main__.py
+++ opcua_server/__main__.py
@@ -1,45 +1,60 @@
 #!/bin/python3
 
-from opcua import ua, Server, instantiate
+print("Importing libraries...")
+
+import asyncio
 import sys
 import time
-sys.path.insert(0, "..")
+from asyncua import ua, Server, uamethod
 from lib.station.Singlebranch import Singlebranch
 
-if __name__ == "__main__":
-    # setup our server
-    server = Server()
+print("Libraries imported!")
+
+stations = []
+
+async def init(server):
+    print("Initializing server")
+    await server.init()
     server.set_endpoint("opc.tcp://0.0.0.0:4840/")
     server.set_server_name("Argus OPC Server");
-    server.set_security_policy([ua.SecurityPolicyType.NoSecurity])
-    #server.load_certificate("./sec/public.der")
+    #server.set_security_policy([ua.SecurityPolicyType.NoSecurity])
+    #await server.load_certificate("./sec/public.der")
     #server.set_security_policy([
     #    ua.SecurityPolicyType.NoSecurity,
     #    ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt,
     #    ua.SecurityPolicyType.Basic256Sha256_Sign])
-    # setup our own namespace, not really necessary but should as spec
     uri = "brahmaget"
-    namespace = server.register_namespace(uri)
+    namespace = await server.register_namespace(uri)
     # get Objects node, this is where we should put our nodes
     objects = server.get_objects_node()
     # populating our address space
     #super_connect = server.nodes.objects.add_folder(namespace, "SuperConnect")
-    super_connect = objects.add_folder(namespace, "SuperConnect")
-    bahiagas = super_connect.add_folder(namespace, "BrahmaGet")
-    stations = []
-    def add_station(region_code, station_code):
+    super_connect = await objects.add_folder(namespace, "SuperConnect")
+    bahiagas = await super_connect.add_folder(namespace, "BrahmaGet")
+    async def add_station(region_code, station_code):
         code_tuple = (region_code, station_code)
-        station = Singlebranch(code_tuple, namespace, bahiagas)
+        station = await Singlebranch(code_tuple, namespace, bahiagas).init()
         stations.append(station)
-    add_station('001', '064')
-    # starting!
-    server.start()
+    await add_station('001', '064')
+
+async def update():
+    print("Updating variables...")
+    [await station.agitate() for station in stations]
+
+def main():
+    print("def main")
+    server = Server()
     try:
-        count = 0
+        asyncio.run(init(server))
         while True:
-            time.sleep(2)
-            [station.agitate() for station in stations]
+            asyncio.run(update())
+            time.sleep(1)
     finally:
-        server.stop()
+        print("finally")
+        asyncio.run(server.stop())
+        exit()
 
+if __name__ == "__main__":
+    main()
 
+print("Script end")
--- opcua_server/lib/station/Singlebranch.py
+++ opcua_server/lib/station/Singlebranch.py
@@ -3,18 +3,24 @@ from lib.station.Station import Station
 class Singlebranch(Station):
     def __init__(self, code_tuple, namespace, parent_node):
         super().__init__(code_tuple, namespace, parent_node)
-        self.init_instruments()
-    def init_instruments(self, number='101'):
-        for isa_letter in instrument_tags:
-            super().add_instrument(isa_letter, number)
+    async def init(self):
+        await super().init()
+        await self.init_instruments()
+        return self
+    async def init_instruments(self, number='201'):
+        for (isa_letter, suffix_number) in instrument_tags:
+            await super().add_instrument(isa_letter, suffix_number)
 
 instrument_tags = [
-    "PI",
-    "PA",
-    "TI",
-    "EI",
-    "FI",
-    "FQI",
-    "FQIA"
+    ("EI", "201"),
+    ("FI", "201"),
+    ("FQI", "201"),
+    ("FQIA", "201"),
+    ("FQIM", "201"),
+    ("FQIMA", "201"),
+    ("PDI", "203"),
+    ("PI", "201"),
+    ("PI", "202"),
+    ("TI", "202")
 ]
 
