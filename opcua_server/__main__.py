2d76fc431007831a3f038ea2c8b17c219a9c2ee0
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-05-20 22:23:09 -0300

    lead certificate first

--- opcua_server/__main__.py
+++ opcua_server/__main__.py
@@ -17,6 +17,7 @@ async def init(server):
     await server.init()
     server.set_endpoint("opc.tcp://0.0.0.0:4840/")
     server.set_server_name("Argus OPC Server");
+    await server.load_certificate("./security/certs/server.der")
     server.set_security_policy(
         [
             ua.SecurityPolicyType.NoSecurity,
@@ -24,7 +25,6 @@ async def init(server):
             ua.SecurityPolicyType.Basic256Sha256_Sign,
         ]
     )
-    await server.load_certificate("./security/certs/server.der")
     #server.set_security_policy([
     #    ua.SecurityPolicyType.NoSecurity,
     #    ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt,
