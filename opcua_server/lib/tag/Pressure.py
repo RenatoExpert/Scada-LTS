32dcbedf99fb04480c20a7616a27f26652040d62
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-05-21 14:33:40 -0300

    Revert "implementing async io server"
    This reverts commit 34b5c6177c0ec95e771ad6d26439acf016c6902d.

--- opcua_server/lib/tag/Pressure.py
+++
@@ -1,4 +0,0 @@
-class pressure(Tag):
-    def __init__(self, station, namespace, var_range=(0, 50), initial_value=20, isa_letter="PI"):
-        super().__init__(station, namespace, var_range, initial_value, isa_letter)
-
