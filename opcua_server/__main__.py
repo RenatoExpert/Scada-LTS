543f83f5d8d812bb229ced1cc73f867c3485c4c3
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-05-20 22:24:43 -0300

    fix certificate name

--- opcua_server/__main__.py
+++ opcua_server/__main__.py
@@ -17,7 +17,7 @@ async def init(server):
     await server.init()
     server.set_endpoint("opc.tcp://0.0.0.0:4840/")
     server.set_server_name("Argus OPC Server");
-    await server.load_certificate("./security/certs/server.der")
+    await server.load_certificate("./security/certs/myserver.der")
     server.set_security_policy(
         [
             ua.SecurityPolicyType.NoSecurity,
