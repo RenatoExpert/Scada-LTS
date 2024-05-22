0940de78b619b8bb818929ba9e9eea5d20e99244
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-05-22 12:26:54 -0300

    making double branch stations

--- opcua_server/lib/station/Singlebranch.py
+++ opcua_server/lib/station/Singlebranch.py
@@ -1,23 +1,23 @@
 from lib.station.Station import Station
 
 class Singlebranch(Station):
+    instrument_tags = [
+        ("EI", "201"),
+        ("FI", "201"),
+        ("FQI", "201"),
+        ("FQIA", "201"),
+        ("FQIM", "201"),
+        ("FQIMA", "201"),
+        ("PDI", "203"),
+        ("PI", "201"),
+        ("PI", "202"),
+        ("TI", "202")
+    ]
     def __init__(self, code_tuple, namespace, parent_node):
         super().__init__(code_tuple, namespace, parent_node)
         self.init_instruments()
-    def init_instruments(self, number='101'):
-        for (isa_letter, number) in instrument_tags:
+    def init_instruments(self):
+        for (isa_letter, number) in self.instrument_tags:
             super().add_instrument(isa_letter, number)
 
-instrument_tags = [
-    ("EI", "201"),
-    ("FI", "201"),
-    ("FQI", "201"),
-    ("FQIA", "201"),
-    ("FQIM", "201"),
-    ("FQIMA", "201"),
-    ("PDI", "203"),
-    ("PI", "201"),
-    ("PI", "202"),
-    ("TI", "202")
-]
 
